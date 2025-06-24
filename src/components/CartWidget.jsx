afc703d8b6cd5422a063ae400ed0031c1be3a2ca
Njsc99 <nicolasj.saezc@gmail.com>
2025-06-24 15:32:56 -0400

    Avance Entrega 1 - NavBar

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import carrito from "../assets/carrito.png";
+
+const CartWidget = () => {
+  return (
+    <div className="cart-widget">
+        <img src={carrito} alt="" />
+        <h2>6</h2>
+    </div>
+  );
+}
+
+export default CartWidget;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import "./NavBar.css";
+import logo from "../assets/logo.jpg";
+import CartWidget from "./CartWidget";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+        <img className="logo" src={logo} alt="" />
+
+        <ul className="nav-list">
+            <li>Teclados</li>
+            <li>Mouse</li>
+            <li>Monitores</li>
+            <li>Accesorios</li>
+        </ul>
+
+        <CartWidget />
+    </div>
+  )
+}
+
+export default NavBar;
